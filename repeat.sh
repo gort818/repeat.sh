fd17163daf38442272f04ef1729248e3bed6e349
Alessandro <gort818@gmail.com>
2016-05-02 21:46:55 -0700

    Update repeat.sh

--- repeat.sh
+++ repeat.sh
@@ -1,35 +1,31 @@
-
-#!/bin/bash
+!/bin/bash
 # Simple bash script to allow one to run a command multiple times
 # The first argument is the amount of this you would like the command to run.
-# The second argument is the command you would like to run, it must be encapsulated in quotation marks
+# The second argument is the command you would like to run.
 
 start=$(date +%s.%N)
 
-if [ $# -ne 2 ]
-	then
-	echo "Wrong number of arguments." 1>&2
-	echo "Usage: $0 <integer> <bash command * use quotation marks to encapsulate the command>" 1>&2
-	exit 1
 
 
-  else
-	if  [[ $1 = *[!0-9]* ]];
+if [[ $1 = *[!0-9]* ]];
 		then
 		echo "First argument must be an integer"
 		exit 2 # not a valid integer 
-	fi
-fi
-count=$1
-echo "
-	The command will be repeated $count times
-     "
+  
 
-command=$(echo "$2")
+
+fi
+
+
+count=$1
+printf "\nThe command will be repeated $count times\n\n"
+
+shift
 
 while [ $count -ge 1 ]
 do
-	 eval $command
+	 
+	"$@"
 	count=$(( count-1 ))
 	
 done
