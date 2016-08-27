159c1f5f5d23a0e94f0386c6345887becd2b4d75
Alessandro <gort818@gmail.com>
2016-08-26 19:42:22 -0700

    Update repeat.sh

--- repeat.sh
+++ repeat.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 # Simple bash script to allow one to run a command multiple times
 # The first argument is the amount of this you would like the command to run.
 # The second argument is the command you would like to run.
